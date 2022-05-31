67420e4fa9c8293bb3170f9754e1356cd163e6d1
elianiva <dicha.arkana03@gmail.com>
2022-05-31 13:36:03 +0700

    chore: mark external packages

--- build.js
+++ build.js
@@ -11,7 +11,7 @@ try {
         sourcemap: true,
         format: "esm",
         target: "esnext",
-        external: ["__STATIC_CONTENT_MANIFEST"],
+        external: ["__STATIC_CONTENT_MANIFEST", "highlight.js", "nanoid"],
         conditions: ["worker", "browser"],
         entryPoints: [path.join(__dirname, "src", "index.ts")],
         outdir: path.join(__dirname, "dist"),
