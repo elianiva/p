0f14f05a5871c551e62db715cf5feb5f6dd7eb0a
elianiva <dicha.arkana03@gmail.com>
2022-05-31 18:09:18 +0700

    refactor: cache NewPaste page

--- src/application/NewPasteView.ts
+++ src/application/NewPasteView.ts
@@ -9,12 +9,16 @@ export class NewPasteView implements IRoute {
     public readonly path = "/";
     public readonly method = "GET";
 
+    private readonly CACHE_DURATION = 60 * 60 * 24; // 1 day
+
     public async handler(
         request: Request,
         env: IEnvironment,
         ctx: ExecutionContext
     ): Promise<Response> {
         const view = new Html(newPasteViewTemplate).minify().content;
-        return new View(view);
+        return new View(view, 200, {
+            "Cache-Control": `max-age=${this.CACHE_DURATION}`,
+        });
     }
 }
